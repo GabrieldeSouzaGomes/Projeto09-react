48459d5fc006b1db3d450736adbe8a39f78995ee
GabrieldeSouzaGomes <166565269+GabrieldeSouzaGomes@users.noreply.github.com>
2024-12-21 19:47:02 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Cronometro from "./components/Cronometro"
+
+function App() {
+  return(
+    <div className="App">
+      <Cronometro/>
+
+    </div>
+  )
+}
+
+export default App
